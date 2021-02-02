69d82c0ca6179bea5233bb7fb522a22981847b02
avnoto <alexandravnoto@gmail.com>
2021-02-02 15:02:06 -0600

    changes to bio and portfolio page

--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -1,82 +1,83 @@
-import React from 'react';
-import './style.css';
-import DSC03189 from '../../assets/images/DSC03189(2).JPG';
-import iconfinder_mark from '../../assets/images/iconfinder_mark-github_298822.png';
-import iconfinder_Stack_Overflow_1298710 from '../../assets/images/iconfinder_Stack_Overflow_1298710.png';
-import iconfinder_square from '../../assets/images/iconfinder_square-linkedin_317725.png';
+import React from "react";
+import "./style.css";
+import DSC03189 from "../../assets/images/DSC03189(2).JPG";
+import iconfinder_mark from "../../assets/images/iconfinder_mark-github_298822.png";
+import iconfinder_Stack_Overflow_1298710 from "../../assets/images/iconfinder_Stack_Overflow_1298710.png";
+import iconfinder_square from "../../assets/images/iconfinder_square-linkedin_317725.png";
 
 function About() {
   return (
-    <div className='jumbotron big-banner'>
-      <div className='container'>
-        <div className='row mt-3'>
-          <div className='col-xs-12 col-sm-7 m-2 custom-panel'>
-            <div className='page-header pt-4 pl-3'>
+    <div className="jumbotron big-banner">
+      <div className="container">
+        <div className="row mt-3">
+          <div className="col-xs-12 col-sm-7 m-2 custom-panel">
+            <div className="page-header pt-4 pl-3">
               <h1>About Me</h1>
             </div>
             <hr />
-            <div className='row'>
-              <div className='col-xs-12 col-sm-5 mt-3'>
+            <div className="row">
+              <div className="col-xs-12 col-sm-5 mt-3">
                 <img
                   src={DSC03189}
-                  alt='Alexandra Noto'
-                  className='img-thumbnail'
+                  alt="Alexandra Noto"
+                  className="img-thumbnail"
                 />
               </div>
-              <div className='col-xs-12 col-sm-7 mt-3 pb-4'>
+              <div className="col-xs-12 col-sm-7 mt-3 pb-4">
                 <p>
-                  I am a student in a full-stack web development bootcamp at UCD
-                  Extension, on track to graduate in September of 2020. I have a
-                  passion for technology and a strong will to learn. Previously,
-                  I worked in the hospitality industry for 6 years, and
-                  currently, I have my own e-commerce business. I plan to have a
-                  full-time career as a software developer. Some hobbies of mine
-                  include travelling, learning new languages, playing music,
-                  graphic design, and gaming.
+                  I am a driven web developer with a passion for technology and
+                  a recent graduate of the Full Stack Web Development program at
+                  UCD. I worked in the hospitality industry for 6 years, and
+                  currently, I have my own e-commerce business. I am searching
+                  for a web developer position and looking forward to being part
+                  of a team that is highly motivated to build the best possible
+                  web experiences. When I'm not developing, I like to travel,
+                  learn new languages, play music, design graphics, and play
+                  video games.
                 </p>
               </div>
             </div>
           </div>
 
-          <div className='col'></div>
+          <div className="col"></div>
 
-          <div className='col-xs-12 col-sm-4'>
-            <div className='card mt-2'>
-              <div className='card-body text-center'>
-                <h4 className='card-title'>Connect with Me</h4>
+          <div className="col-xs-12 col-sm-4">
+            <div className="card mt-2">
+              <div className="card-body text-center">
+                <h4 className="card-title">Connect with Me</h4>
                 <hr />
                 <p>
                   <a
-                    href='https://github.com/avnoto'
-                    target='_blank'
-                    rel='noopener noreferrer'
+                    href="https://github.com/avnoto"
+                    target="_blank"
+                    rel="noopener noreferrer"
                   >
                     <img
                       src={iconfinder_mark}
-                      className='social p-2'
-                      alt='Github'
+                      className="social p-2"
+                      alt="Github"
                     />
                   </a>
                   <a
-                    href='https://www.linkedin.com/in/alexandra-noto-2a44861a6?trk=people-guest_people_search-card'
-                    target='_blank'
-                    rel='noopener noreferrer'
+                    href="https://www.linkedin.com/in/alexandra-noto-2a44861a6?trk=people-guest_people_search-card"
+                    target="_blank"
+                    rel="noopener noreferrer"
                   >
                     <img
                       src={iconfinder_square}
-                      className='social p-2'
-                      alt='LinkedIn'
+                      className="social p-2"
+                      alt="LinkedIn"
                     />
                   </a>
                   <a
-                    href='https://stackoverflow.com/users/13124073/avnoto'
-                    target='_blank'
-                    rel='noopener noreferrer'
+                    href="https://stackoverflow.com/users/13124073/avnoto"
+                    target="_blank"
+                    rel="noopener noreferrer"
                   >
                     <img
                       src={iconfinder_Stack_Overflow_1298710}
-                      className='social p-2'
-                      alt='Stack Overflow'
+                      className="social p-2"
+                      alt="Stack Overflow"
                     />
                   </a>
                 </p>
--- client/src/components/Portfolio/index.js
+++ client/src/components/Portfolio/index.js
@@ -1,212 +1,216 @@
-import React from 'react';
-import './style.css';
-import iconfinder_mark from '../../assets/images/iconfinder_mark-github_298822.png';
-import iconfinder_Stack_Overflow_1298710 from '../../assets/images/iconfinder_Stack_Overflow_1298710.png';
-import iconfinder_square from '../../assets/images/iconfinder_square-linkedin_317725.png';
-import wordgamescrnshot from '../../assets/images/wordgamescrnshot.png';
-import codequizscrnshot from '../../assets/images/codequizscrnshot.png';
-import dayplannerpic1 from '../../assets/images/dayplannerpic1.png';
-import weatherapppic from '../../assets/images/weather-app-scrnshot1.png';
-import playpic from '../../assets/images/2PLAYimg.png';
-import burger from '../../assets/images/burgerimage.png';
-import booksearch from '../../assets/images/book-search.png';
+import React from "react";
+import "./style.css";
+import iconfinder_mark from "../../assets/images/iconfinder_mark-github_298822.png";
+import iconfinder_Stack_Overflow_1298710 from "../../assets/images/iconfinder_Stack_Overflow_1298710.png";
+import iconfinder_square from "../../assets/images/iconfinder_square-linkedin_317725.png";
+import wordgamescrnshot from "../../assets/images/wordgamescrnshot.png";
+import codequizscrnshot from "../../assets/images/codequizscrnshot.png";
+import dayplannerpic1 from "../../assets/images/dayplannerpic1.png";
+import weatherapppic from "../../assets/images/weather-app-scrnshot1.png";
+import playpic from "../../assets/images/2PLAYimg.png";
+import studyCheckImg from "../../assets/images/studyCheckImg.png";
+import booksearch from "../../assets/images/book-search.png";
 
 function Portfolio() {
   return (
-    <div className='jumbotron big-banner'>
+    <div className="jumbotron big-banner">
       {/*This container includes the Portfolio Section and the card.*/}
-      <div className='container'>
-        <div className='row mt-3 mb-0'>
-          <div className='col-xs-12 col-sm-7 m-2 custom-panel'>
-            <div className='page-header pt-4 pl-3'>
+      <div className="container">
+        <div className="row mt-3 mb-0">
+          <div className="col-xs-12 col-sm-7 m-2 custom-panel">
+            <div className="page-header pt-4 pl-3">
               <h1>Portfolio</h1>
             </div>
             <hr />
-            <div className='row'>
-              <div className='col-xs-12 col-sm-3 mt-3 mr-5'>
-                <ul className='caption-style-4'>
+            <div className="row">
+              <div className="col-xs-12 col-sm-3 mt-3 mr-5">
+                <ul className="caption-style-4">
                   <li>
                     <img
                       src={wordgamescrnshot}
-                      alt='Word Guess Game'
-                      className='image'
+                      alt="Word Guess Game"
+                      className="image"
                     />
-                    <div className='caption'>
-                      <a href='https://avnoto.github.io/Word-Guess-Game/'>
-                        <div className='blur' />
-                        <div className='caption-text'>
+                    <div className="caption">
+                      <a href="https://avnoto.github.io/Word-Guess-Game/">
+                        <div className="blur" />
+                        <div className="caption-text">
                           <h3>Word Guess Game</h3>
                         </div>
                       </a>
                     </div>
-                    <a href='https://avnoto.github.io/Word-Guess-Game/'></a>
+                    <a href="https://avnoto.github.io/Word-Guess-Game/"></a>
                   </li>
                 </ul>
               </div>
-              <div className='col-xs-12 col-sm-3 mt-3 ml-5'>
-                <ul className='caption-style-4'>
+              <div className="col-xs-12 col-sm-3 mt-3 ml-5">
+                <ul className="caption-style-4">
                   <li>
                     <img
                       src={codequizscrnshot}
-                      alt='Code Quiz'
-                      className='image'
+                      alt="Code Quiz"
+                      className="image"
                     />
-                    <div className='caption'>
-                      <a href='https://avnoto.github.io/Code-Quiz/'>
-                        <div className='blur' />
-                        <div className='caption-text'>
+                    <div className="caption">
+                      <a href="https://avnoto.github.io/Code-Quiz/">
+                        <div className="blur" />
+                        <div className="caption-text">
                           <h3>Code Quiz</h3>
                         </div>
                       </a>
                     </div>
-                    <a href='https://avnoto.github.io/Code-Quiz/'></a>
+                    <a href="https://avnoto.github.io/Code-Quiz/"></a>
                   </li>
                 </ul>
               </div>
             </div>
             <br />
             <br />
-            <div className='row'>
-              <div className='col-xs-12 col-sm-3 my-3 mr-5'>
-                <ul className='caption-style-4'>
+            <div className="row">
+              <div className="col-xs-12 col-sm-3 my-3 mr-5">
+                <ul className="caption-style-4">
                   <li>
                     <img
                       src={dayplannerpic1}
-                      alt='Work Day Scheduler'
-                      className='image'
+                      alt="Work Day Scheduler"
+                      className="image"
                     />
-                    <div className='caption'>
-                      <a href='https://avnoto.github.io/Work-Day-Scheduler/'>
-                        <div className='blur' />
-                        <div className='caption-text'>
+                    <div className="caption">
+                      <a href="https://avnoto.github.io/Work-Day-Scheduler/">
+                        <div className="blur" />
+                        <div className="caption-text">
                           <h3>Work Day Scheduler</h3>
                         </div>
                       </a>
                     </div>
-                    <a href='https://avnoto.github.io/Work-Day-Scheduler/'></a>
+                    <a href="https://avnoto.github.io/Work-Day-Scheduler/"></a>
                   </li>
                 </ul>
               </div>
-              <div className='col-xs-12 col-sm-3 my-3 pb-3 ml-5'>
-                <ul className='caption-style-4'>
+              <div className="col-xs-12 col-sm-3 my-3 pb-3 ml-5">
+                <ul className="caption-style-4">
                   <li>
                     <img
                       src={weatherapppic}
-                      alt='Weather Dashboard'
-                      className='image'
+                      alt="Weather Dashboard"
+                      className="image"
                     />
-                    <div className='caption'>
-                      <a href='https://avnoto.github.io/Weather-Dashboard/'>
-                        <div className='blur' />
-                        <div className='caption-text'>
+                    <div className="caption">
+                      <a href="https://avnoto.github.io/Weather-Dashboard/">
+                        <div className="blur" />
+                        <div className="caption-text">
                           <h3>Weather Dashboard</h3>
                         </div>
                       </a>
                     </div>
-                    <a href='https://avnoto.github.io/Weather-Dashboard/'></a>
+                    <a href="https://avnoto.github.io/Weather-Dashboard/"></a>
                   </li>
                 </ul>
               </div>
             </div>
             <br />
-            <div className='row'>
-              <div className='col-xs-12 col-sm-3 my-3 mr-5'>
-                <ul className='caption-style-4'>
+            <div className="row">
+              <div className="col-xs-12 col-sm-3 my-3 mr-5">
+                <ul className="caption-style-4">
                   <li>
-                    <img src={playpic} alt='2PLAY' className='image' />
-                    <div className='caption'>
-                      <a href='https://go-2play.herokuapp.com/'>
-                        <div className='blur' />
-                        <div className='caption-text'>
+                    <img src={playpic} alt="2PLAY" className="image" />
+                    <div className="caption">
+                      <a href="https://go-2play.herokuapp.com/">
+                        <div className="blur" />
+                        <div className="caption-text">
                           <h3>2PLAY</h3>
                         </div>
                       </a>
                     </div>
-                    <a href='https://go-2play.herokuapp.com/'></a>
+                    <a href="https://go-2play.herokuapp.com/"></a>
                   </li>
                 </ul>
               </div>
-              <div className='col-xs-12 col-sm-3 my-3 pb-3 ml-5'>
-                <ul className='caption-style-4'>
+              <div className="col-xs-12 col-sm-3 my-3 pb-3 ml-5">
+                <ul className="caption-style-4">
                   <li>
-                    <img src={burger} alt='EAT-DA-BURGER' className='image' />
-                    <div className='caption'>
-                      <a href='https://eat-da-burger-avnoto.herokuapp.com/'>
-                        <div className='blur' />
-                        <div className='caption-text'>
-                          <h3>EAT-DA-BURGER</h3>
+                    <img
+                      src={studyCheckImg}
+                      alt="Study Check"
+                      className="image"
+                    />
+                    <div className="caption">
+                      <a href="http://study-check.herokuapp.com/">
+                        <div className="blur" />
+                        <div className="caption-text">
+                          <h3>Study Check</h3>
                         </div>
                       </a>
                     </div>
-                    <a href='https://eat-da-burger-avnoto.herokuapp.com/'></a>
+                    <a href="http://study-check.herokuapp.com/"></a>
                   </li>
                 </ul>
               </div>
             </div>
             <br />
-            <div className='row'>
-              <div className='col-xs-12 col-sm-3 my-3 mr-5'>
-                <ul className='caption-style-4'>
+            <div className="row">
+              <div className="col-xs-12 col-sm-3 my-3 mr-5">
+                <ul className="caption-style-4">
                   <li>
                     <img
                       src={booksearch}
-                      alt='Google Books Search'
-                      className='image'
+                      alt="Google Books Search"
+                      className="image"
                     />
-                    <div className='caption'>
-                      <a href='https://avnoto-google-books.herokuapp.com/'>
-                        <div className='blur' />
-                        <div className='caption-text'>
+                    <div className="caption">
+                      <a href="https://avnoto-google-books.herokuapp.com/">
+                        <div className="blur" />
+                        <div className="caption-text">
                           <h3>Google Books Search</h3>
                         </div>
                       </a>
                     </div>
-                    <a href='https://avnoto-google-books.herokuapp.com/'></a>
+                    <a href="https://avnoto-google-books.herokuapp.com/"></a>
                   </li>
                 </ul>
               </div>
             </div>
           </div>
           {/*This is an empty column to give space between the Portfolio section and the card.*/}
-          <div className='col' />
+          <div className="col" />
           {/*This card has social media images that links to my social accounts.*/}
-          <div className='col-xs-12 col-sm-4'>
-            <div className='card mt-2'>
-              <div className='card-body text-center'>
-                <h4 className='card-title'>Connect with Me</h4>
+          <div className="col-xs-12 col-sm-4">
+            <div className="card mt-2">
+              <div className="card-body text-center">
+                <h4 className="card-title">Connect with Me</h4>
                 <hr />
                 <p>
                   <a
-                    href='https://github.com/avnoto'
-                    target='_blank'
-                    rel='noopener noreferrer'
+                    href="https://github.com/avnoto"
+                    target="_blank"
+                    rel="noopener noreferrer"
                   >
                     <img
                       src={iconfinder_mark}
-                      className='social p-2'
-                      alt='Github'
+                      className="social p-2"
+                      alt="Github"
                     />
                   </a>
                   <a
-                    href='https://www.linkedin.com/in/alexandra-noto-2a44861a6?trk=people-guest_people_search-card'
-                    target='_blank'
-                    rel='noopener noreferrer'
+                    href="https://www.linkedin.com/in/alexandra-noto-2a44861a6?trk=people-guest_people_search-card"
+                    target="_blank"
+                    rel="noopener noreferrer"
                   >
                     <img
                       src={iconfinder_square}
-                      className='social p-2'
-                      alt='LinkedIn'
+                      className="social p-2"
+                      alt="LinkedIn"
                     />
                   </a>
                   <a
-                    href='https://stackoverflow.com/users/13124073/avnoto'
-                    target='_blank'
-                    rel='noopener noreferrer'
+                    href="https://stackoverflow.com/users/13124073/avnoto"
+                    target="_blank"
+                    rel="noopener noreferrer"
                   >
                     <img
                       src={iconfinder_Stack_Overflow_1298710}
-                      className='social p-2'
-                      alt='Stack Overflow'
+                      className="social p-2"
+                      alt="Stack Overflow"
                     />
                   </a>
                 </p>
